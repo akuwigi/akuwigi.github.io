64053bb24e08cc0ae1e063ea4c456f63883a61be
akuwigi <120478551+akuwigi@users.noreply.github.com>
2023-02-27 10:25:48 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Wigianto</span>  pelajar yang sedang menempuh pendidikan Sekolah Menengah Kejuruan di sebuah sekolah di Brebes Selatan SMK Negeri 1 Tonjong. Saya mengambil kompetensi di bidang Teknologi Rekayasa dengan kopetensi Teknik Instalasi Tenaga Listrik ! <br><br> Selain itu saya juga memperlajari bidang lain terkhusus di dunia IT sehingga saya dapat membuat sebuah website portofolio pribadi saya yang tidak lain agar dapat mengenalkan diri lebih luas! <br> <br> Saya harap ada saran bagi saya untuk mengembangkan diri <br> Chuss... Click SEND ME !  "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
