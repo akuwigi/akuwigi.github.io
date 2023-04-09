7fad02ba855ca80b27c0b25c4d41dccbf47b3194
Wigii <120478551+akuwigi@users.noreply.github.com>
2023-04-10 04:22:02 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Wigianto</span>  pelajar yang sedang menempuh pendidikan Vokasi dengan  kompetensi di bidang Teknologi dan Rekayasa dengan kopetensi Teknik Instalasi Tenaga Listrik. <br> <br> Saya harap ada saran dan masukan bagi diri saya! "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
